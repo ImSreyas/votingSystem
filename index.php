877b15c27cdc35c0292bc5e5e38f077d6c6708d5
ImSreyas <sreyassatheesh755@gmail.com>
2023-02-25 23:33:14 +0530

    landing page modified

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 
     <div class="main-body">
         <div class="first-container">
-            <div class="first-image-container"><image class="first-image" src='img/Voting-rafiki.svg'/></div>
+            <div class="first-image-container"><image class="first-image" src='img/Vowels-pana.svg'/></div>
             <div class="un-text">Mahatma Gandhi University student's union</div>
         </div>
         <div class="second-container">
             <div class="ex-text"><h>Your vote is your voice</h>Your vote is not just a piece of paper or a checkmark on a ballot, it is the manifestation of your beliefs and the power to make your voice heard. With each vote, you have the ability to shape the direction of your community and country, to demand change and progress, and to hold those in power accountable for their actions.</div>
-            <div class="second-image-container"><image class="second-image" src='img/elections-bg.png'/></div>
+            <div class="second-image-container"><image class="second-image" src='img/Happy student-pana.svg'/></div>
         </div>
     </div>
     
